02e5dfce3cf80d3b94117e263e401800edc40f65
Nazim Boudeffa <boudeffa@gmail.com>
2025-07-01 16:58:06 +0200

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+export default function PP() {
+    return (
+        <main className="m-auto my-10 max-w-5xl space-y-8 px-3 text-center">
+            <h1 className="text-4xl font-bold flex items-center justify-center">
+                About BagBusiness
+            </h1>
+
+            <section>
+                <h2 className="text-2xl font-semibold mb-4 flex items-center justify-center">
+                    What is CabaBusiness?
+                </h2>
+                <p>
+                    CabasBusiness is a platform that let you find products from Cabas entrepreuneurs.
+                </p>
+            </section>
+
+            <p className="mt-6">
+                Thank you for using CabasBusiness!
+            </p>
+        </main>
+    )
+}
